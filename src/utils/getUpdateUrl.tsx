876d1e2def1046eaea165d20748edd8263d65d20
AlanDellonSchwartzman <alandellon0@gmail.com>
2022-03-31 22:48:04 -0300

    fix getUpdateUrl for service

--- src/utils/getUpdateUrl.tsx
+++ src/utils/getUpdateUrl.tsx
@@ -1,44 +1,20 @@
-// import axios from 'axios';
-import { parseString } from 'xml2js';
-
+import axios from 'axios';
 interface GetUpdateUrlProps {
   serial: string;
 }
 
 export default async function getUpdateUrl(props: GetUpdateUrlProps) {
   const { serial } = props;
-  const BASE_URL = `https://a0.ww.np.dl.playstation.net/tpl/np/${serial}/${serial}-ver.xml`;
+  const BASE_URL = `${import.meta.env.VITE_SERVICE_URL}`;
 
   try {
-    // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+    const { data } = await axios.get(BASE_URL, {
+      params: {
+        serial,
+      },
+    });
 
-    // const response = await axios.get(BASE_URL, {
-    //   headers: { 'Content-Type': 'text/xml', 'Sec-Fetch-Mode': 'navigate' },
-    // });
-
-    // const custom_request = new Request(BASE_URL, {
-    //   method: 'GET',
-    //   headers: {
-    //     'Content-Type': 'application/xml',
-    //     'Access-Control-Allow-Origin': '*',
-    //   },
-    // });
-
-    await fetch(BASE_URL, {
-      headers: [
-        ['Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'],
-        ['Content-Type', 'application/xml'],
-        [ 'Ses-Fetch-Mode', 'navigate' ],
-        [ 'Sec-Fetch-Dest',  'document' ],
-        [ 'Sec-Fetch-User',  '?1' ],
-        [ 'Upgrade-Insecure-Requests',  '1' ],
-
-      ],
-      credentials: 'include',
-    })
-      .then((res) => console.log('res', res))
-      .catch((error) => console.log('ERROR', error));
-      
+    return data;
   } catch (error) {
     console.log('ERROR', error);
   }
